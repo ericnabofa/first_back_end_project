f4cf578e1c19842514f98dcce0faef4bd646cffe
ericnabofa <111744463+ericnabofa@users.noreply.github.com>
2023-11-22 12:23:32 +0000

    addind branch 3_get_api to main

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -26,3 +26,40 @@ describe('GET /api/topics', () => {
     });
 });
 
+
+describe('GET /api', () => {
+    test('should respond with the content of endpoints.json', () => {
+      return request(app)
+        .get('/api')
+        .expect(200)
+        .then(({ body }) => {
+          expect(body).toEqual(expectedEndpoints);
+        });
+    });
+  });
+  const db = require('../db/connection')
+const seed = require('../db/seeds/seed')
+const data = require('../db/data/test-data/index')
+const request = require('supertest')
+const app = require('../app/app')
+
+beforeAll(() => seed(data));
+afterAll(() => db.end());
+
+describe('GET /api/topics', () => {
+    test('200: should respond with an array of all topic objects', () => {
+        return request(app)
+        .get('/api/topics')
+        .expect(200)
+        .then(({ body}) => {
+            const  { topics} = body;
+            for (const topic of topics) {
+                expect(topic).toMatchObject({
+                    slug: expect.any(String),
+                    description: expect.any(String)
+                });
+            }
+        })
+    });
+});
+

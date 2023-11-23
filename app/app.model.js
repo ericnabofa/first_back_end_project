930648984d700675a756f74820604c0f3ef3316a
ericnabofa <111744463+ericnabofa@users.noreply.github.com>
2023-11-23 17:29:59 +0000

    adds updates to valid but non existent id on 7_postArticleComment

--- app/app.model.js
+++ app/app.model.js
@@ -58,9 +58,6 @@ exports.insertCommentByArticle_Id = (article_id, username, body) => {
 
     return db.query(queryString, queryValues)
     .then(({rows}) => {
-        if(!rows.length){
-            return Promise.reject({status: 404, msg: 'article does not exist'})
-        }
         return rows[0]
     })
 
